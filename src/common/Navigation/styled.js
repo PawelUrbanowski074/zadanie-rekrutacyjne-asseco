6270487c87a372d0d9af112932f437a30b90b53c
palel <32599663+palel@users.noreply.github.com>
2022-03-20 11:20:31 +0100

    Navigation colors changed to green

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 const activeclassname = "active";
 
 export const List = styled.ul`
-	background-color: white;
+	background-color: #007834;
 	list-style: none;
 	margin: 0px;
 	padding: 0px;
 	display: flex;
 	justify-content: center;
-  box-shadow: 8px 8px 24px -20px rgba(66, 68, 90, 1);
+  box-shadow: 8px 8px 24px -10px rgba(66, 68, 90, 1);
 `;
 
 export const Item = styled.li`
@@ -21,11 +21,11 @@ export const Item = styled.li`
 export const StyledNavLink = styled(NavLink).attrs(() => ({
 	activeclassname,
 }))`
-	color: #007834; 
+	color: white; 
 	text-decoration: none;
 	&.${activeclassname} {
 		font-weight: bold;
-    color: #025a28;
-    border-bottom: 3px solid #025a28;
+    color: white;
+    border-bottom: 3px solid white;
 	}
 `;
