2f6d48a7d5671d92b6901017ae5eab970f0b7037
palel <32599663+palel@users.noreply.github.com>
2022-03-20 11:21:16 +0100

    Model Details component with styles

---
+++ src/features/modelDetails/index.js
@@ -0,0 +1,74 @@
+import { useParams } from "react-router-dom";
+import { Header } from "../../common/Header";
+import { Wrapper } from "../../common/Wrapper/index.js";
+import { selectModelById } from "./selectModelById";
+import { Caption, Table, TableCell, TableContainer, TableRow } from "./styled";
+
+const ModelDetails = ({modelsList}) => {
+
+  const { id } = useParams();
+  const model = selectModelById( modelsList, id);
+  return (
+    <Wrapper>
+      <Header>Model: {model.nazwa} </Header>
+      <TableContainer>
+        <Table>
+          <Caption>Właściowści modelu:</Caption>
+
+          <TableRow>
+            <TableCell>Nazwa modelu:</TableCell>
+            <TableCell>{model.nazwa}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Data:</TableCell>
+            <TableCell>{model.data_na}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Frakcja Testowa:</TableCell>
+            <TableCell>{model.frakcja_testowa}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>N trees:</TableCell>
+            <TableCell>{model.n_trees}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Głębokość interakcji:</TableCell>
+            <TableCell>{model.interaction_depth}</TableCell>
+          </TableRow>
+          
+          <TableRow>
+            <TableCell>Shrinkage:</TableCell>
+            <TableCell>{model.shrinkage}</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>N minobsinnode:</TableCell>
+            <TableCell>{model.n_minobsinnode}</TableCell>
+          </TableRow>
+          
+          <TableRow>
+            <TableCell>CV Folds:</TableCell>
+            <TableCell>{model.cv_folds}</TableCell>
+          </TableRow>
+          
+          <TableRow>
+            <TableCell>Threshold:</TableCell>
+            <TableCell>{model.threshold}</TableCell>
+          </TableRow>
+            
+          <TableRow>
+            <TableCell>Status:</TableCell>
+            <TableCell>{model.status}</TableCell>
+          </TableRow>
+          
+        </Table>
+      </TableContainer>
+    </Wrapper>
+  )
+}
+
+export default ModelDetails;
---
+++ src/features/modelDetails/styled.js
@@ -0,0 +1,50 @@
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
+import styled from "styled-components";
+
+export const TableContainer = styled.div`
+  margin-top: 50px;
+  overflow-x: auto;
+`;
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  width: 80%;
+  margin: auto;
+  color: white;
+  border: 3px solid #02481f;
+  border-radius: 3px;
+  box-shadow: 8px 8px 24px -10px rgba(66, 68, 90, 1);
+`;
+
+export const Caption = styled.caption`
+  color: white;
+  background-color: #025a28;
+  text-align: left;
+  margin: 15px 0px;
+  padding: 10px 20px;
+  border: 3px solid #02481f;
+  border-radius: 3px;
+  font-size: 22px;
+  font-weight: bold;
+  box-shadow: 8px 8px 24px -10px rgba(66, 68, 90, 1);
+
+`;
+
+export const TableRow = styled.tr`
+  background-color: #025a28;
+
+  &:nth-child(even) {
+    filter: brightness(110%);
+  }
+
+  &:hover {
+    filter: brightness(120%);
+  }
+`;
+
+export const TableCell = styled.td`
+  border: 3px solid #02481f;
+  padding: 15px;
+  text-align: center;
+  vertical-align: middle;
+`;
