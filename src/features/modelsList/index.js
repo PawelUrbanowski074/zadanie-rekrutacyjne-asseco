43196b7de37259111e61c7ebe2f3f3c4bc3a0064
palel <32599663+palel@users.noreply.github.com>
2022-03-21 21:18:25 +0100

    dowload models button added

--- src/features/modelsList/index.js
+++ src/features/modelsList/index.js
@@ -2,21 +2,23 @@ import { Header } from "../../common/Header";
 import { Wrapper } from "../../common/Wrapper/index.js";
 import { Link } from "react-router-dom";
 
-import { Button, Item, List, ModelName, Section, StyledLink } from "./styled";
+import { Button, Buttons, Item, List, ModelName, Section, StyledLink } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
 import { removeModel, selectModels } from "../modelsSlice";
+import DownloadModelsButton from "./downloadModelsButton";
 
 
 const ModelsList = () => {
 
-  const modelsList = useSelector( state => selectModels(state));
+  const modelsList = useSelector(state => selectModels(state));
   const dispatch = useDispatch();
-
+  console.log(modelsList);
   return (
     <Wrapper>
       <Header> Lista modeli</Header>
 
       <Section>
+        <DownloadModelsButton />
         <List>
           {modelsList.map(model => (
             <Item
@@ -25,12 +27,16 @@ const ModelsList = () => {
               <ModelName>
                 <StyledLink to={`modelsList/${model.id}`}>{model.nazwa}</StyledLink>
               </ModelName>
-              <Link to={`modelsList/${model.id}`}>
-                <Button>Pokaż szczegóły</Button>
-              </Link>
-              <Button
-                onClick={() => dispatch(removeModel(model.id))}
-              >Usuń model</Button>
+              <Buttons>
+                <Link to={`modelsList/${model.id}`}>
+                  <Button>Pokaż szczegóły</Button>
+                </Link>
+                <Button
+                  onClick={() => dispatch(removeModel(model.id))}
+                >
+                  Usuń model
+                </Button>
+              </Buttons>
             </Item>
           ))}
         </List>
