97a48d4e02cd8ad857b2949a3af9cbb9114ef32c
palel <32599663+palel@users.noreply.github.com>
2022-03-22 23:18:47 +0100

    unused array deleted, Authentications usage

--- src/App.js
+++ src/App.js
@@ -1,64 +1,39 @@
+import { useSelector } from "react-redux";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import Navigation from "./common/Navigation";
 import AddModel from "./features/addModel";
-
 import Authentications from "./features/authentication";
 import ModelDetails from "./features/modelDetails";
 import ModelsList from "./features/modelsList";
 import { toModel, toModels, toNewModel } from "./routes";
+import { selectIsLogged } from "./features/modelsSlice";
 
+const App = () => {
+  const logged = useSelector(selectIsLogged);
 
-const modelsList = [
-  {
-    "id": 1,
-    "nazwa": "model_ko.1.01.007",
-    "data_na": "2020-03-13",
-    "frakcja_testowa": 0.3,
-    "n_trees": 5,
-    "interaction_depth": 10,
-    "shrinkage": 0.035,
-    "n_minobsinnode": 15,
-    "cv_folds": 2,
-    "threshold": 0.5,
-    "status": "ready"
-  },
-  {
-    "id": 2,
-    "nazwa": "model_ko.1.01.008",
-    "data_na": "2020-03-14",
-    "frakcja_testowa": 0.4,
-    "n_trees": 6,
-    "interaction_depth": 11,
-    "shrinkage": 0.045,
-    "n_minobsinnode": 20,
-    "cv_folds": 3,
-    "threshold": 0.6,
-    "status": "new"
-  }
-];
-const App = () => (
-  <HashRouter>
-    <Navigation />
-    <Switch>
-      <Route path={toModel()}>
-        <ModelDetails modelsList={modelsList}/>
-      </Route>
-      <Route path={toModels()}>
-        <ModelsList modelsList={modelsList} />
-      </Route>
-      <Route path={toNewModel()}>
-        <AddModel />
-      </Route>
-      <Route path="/">
-        <Redirect to={toModels()}/>
-      </Route>
+  if(!logged)
+    return <Authentications />
+  else
+    return (
+      <HashRouter>
+        <Navigation />
+        <Switch>
+          <Route path={toModel()}>
+            <ModelDetails />
+          </Route>
+          <Route path={toModels()}>
+            <ModelsList />
+          </Route>
+          <Route path={toNewModel()}>
+            <AddModel />
+          </Route>
+          <Route path="/">
+            <Redirect to={toModels()} />
+          </Route>
 
-    </Switch>
-  </HashRouter>
-
-  // <Wrapper>
-  //   <Authentications />
-  // </Wrapper>
-)
+        </Switch>
+      </HashRouter>
+    )
+}
 
 export default App;
