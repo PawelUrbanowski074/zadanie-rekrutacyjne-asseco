fc47ca81682dc90c73b889e1930784bc17765879
palel <32599663+palel@users.noreply.github.com>
2022-03-22 23:16:48 +0100

    dispatch login action on form submit

--- src/features/authentication/index.js
+++ src/features/authentication/index.js
@@ -1,28 +1,33 @@
 import { Header } from "../../common/Header";
 import { Wrapper } from "../../common/Wrapper/index.js";
 import { Arrow, Button, ButtonText, Container, Field, Form, Input, Title } from "../../common/Form";
+import { useDispatch } from "react-redux";
+import { fetchLogin } from "../modelsSlice";
 
-const Authentications = () => (
-  <Wrapper>
-    <Header>Logowanie</Header>
-    <Container>
-      <Title>Zaloguj do serwisu</Title>
-      <Form>
-        <Field>
-          Login
-          <Input />
-        </Field>
-        <Field>
-          Hasło
-          <Input />
-        </Field>
-        <Button>
-          <ButtonText>Zaloguj</ButtonText>
-          <Arrow>{'>'}</Arrow>
-        </Button>
-      </Form>
-    </Container>
-  </Wrapper>
-)
+const Authentications = () => {
+const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      <Header>Logowanie</Header>
+      <Container>
+        <Title>Zaloguj do serwisu</Title>
+        <Form onSubmit={() => dispatch(fetchLogin())}>
+          <Field>
+            Login
+            <Input required />
+          </Field>
+          <Field>
+            Hasło
+            <Input type="password" required />
+          </Field>
+          <Button>
+            <ButtonText >Zaloguj</ButtonText>
+            <Arrow>{'>'}</Arrow>
+          </Button>
+        </Form>
+      </Container>
+    </Wrapper>
+  )
+}
 
 export default Authentications;
