81027fbf95e757e69696e5e8643bf816e8e67d80
palel <32599663+palel@users.noreply.github.com>
2022-03-23 21:42:30 +0100

    FraudResult styled component

---
+++ src/features/modelDetails/FraudResult/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const FraudResult = styled.div`
+  text-align: center;
+  font-size: 18px;
+  color: white;
+  background-color: #025a28;
+  margin-top: 30px;
+  padding: 10px 0;
+  border-radius: 3px;
+  box-shadow: 8px 8px 24px -10px rgba(66, 68, 90, 1);
+
+  @media(max-width: 767px) {
+    margin: 20px 0;
+  }
+`;
