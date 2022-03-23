a7172748639cf6ff87e50ffd40b49b4966cde65a
palel <32599663+palel@users.noreply.github.com>
2022-03-23 21:55:47 +0100

    loggedIn parameth added to localStorage

--- src/features/localStorage.js
+++ src/features/localStorage.js
@@ -1,6 +1,10 @@
 const localStorageKey = "models";
+const loggedInKey = "loggedIn"; 
 
 export const saveModelsInLocalStorage = models => localStorage.setItem(localStorageKey, JSON.stringify(models));
 
-export const getModelsFromLocalStorage = () => JSON.parse(localStorage.getItem(localStorageKey)) || 
-  [];
+export const getModelsFromLocalStorage = () => JSON.parse(localStorage.getItem(localStorageKey)) || [];
+
+  export const saveIsLoggedInLocalStorage = isLogged => localStorage.setItem(loggedInKey, JSON.stringify(isLogged));
+
+  export const getIsLoggedFromLocalStorage = () => JSON.parse(localStorage.getItem(loggedInKey)) || false;
